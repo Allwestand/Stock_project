e0929bbbbd5b65f5a518f98a7c31f351045c136d
Tanee <Allwestandz@gmail.com>
2023-11-01 05:07:49 +0700

    first flask

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages,setup
+
+
+setup(
+     name = 'Stock_project',
+     version='0.1',
+     description='Project practice machine learning,Data analysis and so on',
+     author='Tanee',
+     author_email='Allwestandz@gmail.com',
+     packages=find_packages(),
+     install_requires=['pandas','numpy']
+     
+
+
+
+
+
+)
